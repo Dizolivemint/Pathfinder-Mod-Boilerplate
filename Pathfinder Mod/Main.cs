f7641fc98be2a07af3c3f639b53e2d7d9943d7b5
Miles Exner <dizolivemint@gmail.com>
2019-03-24 18:15:55 -0700

    [Add] From examples

---
+++ Pathfinder Mod/Main.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Harmony;
+using UnityModManagerNet;
+
+namespace TestMod
+{
+    static class Main
+    {
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            modEntry.Logger.Log(Application.loadedLevelName);
+
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Application), "loadedLevelName", MethodType.Getter)]
+    static class Application_loadedLevelName_Patch
+    {
+        static void Postfix(ref string __result)
+        {
+            if (!Main.enabled)
+                return;
+
+            __result = "New Level Name";
+        }
+    }
+}
